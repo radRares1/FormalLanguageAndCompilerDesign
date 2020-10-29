6f89320c0422151121cbc930a7c3b6446160dae4
RavaszTamas <52380110+RavaszTamas@users.noreply.github.com>
2020-10-29 10:25:14 +0200

    Ravasz Tamas lab 3 review
    Nothing to add, pif looks ok

--- lab2/src/main/scala/ProgramInternalForm.scala
+++ lab2/src/main/scala/ProgramInternalForm.scala
@@ -2,7 +2,7 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 case class ProgramInternalForm(){
-
+//PIF looks ok
   val content:mutable.HashMap[String,Int] = mutable.HashMap.empty
 
   def add(entry:String,stPosition:Integer):Unit = {
