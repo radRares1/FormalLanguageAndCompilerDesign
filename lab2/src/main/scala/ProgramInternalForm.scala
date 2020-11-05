eac347c2b005b1ec7b5c621e854ac32d6b3e5ea7
RavaszTamas <52380110+RavaszTamas@users.noreply.github.com>
2020-11-05 17:14:57 +0200

    Review for lab4
    Correct fix for the PIF by using the tokens and not the code for them

--- lab2/src/main/scala/ProgramInternalForm.scala
+++ lab2/src/main/scala/ProgramInternalForm.scala
@@ -6,6 +6,7 @@ case class ProgramInternalForm(){
 
   val content:ListBuffer[Tuple2[String,Int]] = ListBuffer.empty
 
+  
   def add(entry:String,stPosition:Integer):Unit = {
     content.addOne((entry,stPosition))
   }
